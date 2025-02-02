9a3c8fb4226ccc121324067c6cb8df6ea9521691
preetikadian <preeti.ktokas@gmail.com>
2025-02-02 22:11:08 +1100

    Added test methods for Get and Put request and included Testng

--- src/files/ReUsableMethods.java
+++ src/files/ReUsableMethods.java
@@ -5,7 +5,6 @@ import io.restassured.path.json.JsonPath;
 public class ReUsableMethods {
 
      public static JsonPath rawToJson(String response){
-         JsonPath js = new JsonPath(response);
-         return js;
+         return new JsonPath(response);
      }
 }
